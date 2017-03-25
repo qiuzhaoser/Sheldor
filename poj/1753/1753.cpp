83b7e7999868b155be9b85425699fb2544f37df6
Zhao Qiu <sheldon@Zhaos-MacBook-Air.local>
2017-03-26 00:11:34 +0800

    add a manuscrip fro poj 1753

---
+++ poj/1753/1753.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+bool array[4][4];
+int main()
+{
+	char c;
+	for( int i = 0; i < 4; i++ )
+	{
+		for( int j = 0; j < 4; j++ )
+		{
+			scanf("%c",&ch);
+			array[i][j] = 'w' == c ? false:true;
+		}
+	}
+	cout<<"hello world"<<endl;
+	return 0;
+}
